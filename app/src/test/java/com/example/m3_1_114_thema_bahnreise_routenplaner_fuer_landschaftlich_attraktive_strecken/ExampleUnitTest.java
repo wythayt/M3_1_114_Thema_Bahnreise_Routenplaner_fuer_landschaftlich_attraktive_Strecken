7f3addf557926a3dde4049376fa428e762baa39d
Darya Haponava <a52209292@unet.univie.ac.at>
2025-05-10 13:52:46 +0200

    Initial commit

---
+++ app/src/test/java/com/example/m3_1_114_thema_bahnreise_routenplaner_fuer_landschaftlich_attraktive_strecken/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.m3_1_114_thema_bahnreise_routenplaner_fuer_landschaftlich_attraktive_strecken;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
