f80da33baa99bcd32b31477a8e1ab6b3fac22a5f
Anastasiia Zimanova <zimnasty02@gmail.com>
2025-05-15 13:16:04 +0200

    Add: details screen, Fix: search screen

---
+++ app/src/main/java/com/example/m3_app/ui/search/DetailsFragment.java
@@ -0,0 +1,29 @@
+package com.example.m3_app.ui.search;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.m3_app.databinding.FragmentDetailsBinding;
+
+public class DetailsFragment extends Fragment {
+    private FragmentDetailsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentDetailsBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
