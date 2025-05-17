4caff7346376ca2592da6f270fa8e4c27a9517f8
Aleksandr Kogan <a12238680@unet.univie.ac.at>
2025-05-17 19:00:57 +0200

    feat: Add HelpFragment

--- app/src/main/java/com/example/m3_app/ui/help/HelpFragment.java
+++ app/src/main/java/com/example/m3_app/ui/help/HelpFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -23,8 +22,6 @@ public class HelpFragment extends Fragment {
         binding = FragmentHelpBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHelp;
-        helpViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
